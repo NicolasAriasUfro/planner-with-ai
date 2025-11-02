62f0e21ecd9ad7c090afec7ede8cb0fcef44a1f0
Alex Sagredo Burgos <alexsagredo1204@gmail.com>
2025-11-01 23:27:02 -0300

    Creación de base para las pruebas en cypress

---
+++ test/smoke_test.cy.js
@@ -0,0 +1,8 @@
+const URL = 'http://localhost:3000';
+
+describe('Pruebas de humo para el schedule de OpenRouter', () => {
+
+    it('Debería generar un plan de estudio utilizando OpenRouter', () => {
+
+    });
+});
